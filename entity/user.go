faa62843abcf958be2c4258fe753c932ade1fd9e
mkaiho <mkaiho.cgengo@gmail.com>
2022-11-02 15:02:13 +0000

    add user entity

---
+++ entity/user.go
@@ -0,0 +1,33 @@
+package entity
+
+type UserID string
+
+func NewUser(
+	id UserID,
+	name string,
+	email string,
+) *User {
+	return &User{
+		id:    id,
+		name:  name,
+		email: email,
+	}
+}
+
+type User struct {
+	id    UserID
+	name  string
+	email string
+}
+
+func (u *User) ID() UserID {
+	return u.id
+}
+
+func (u *User) Name() string {
+	return u.name
+}
+
+func (u *User) Email() string {
+	return u.email
+}
