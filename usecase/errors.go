bb636d9f031041ca17b5ac165d94a192f7869672
mkaiho <mkaiho.cgengo@gmail.com>
2022-11-05 06:19:29 +0000

    add DuplicateError

--- usecase/errors.go
+++ usecase/errors.go
@@ -18,3 +18,18 @@ func NewNotFoundError(targetName string) *NotFoundEntityError {
 func (e *NotFoundEntityError) Error() string {
 	return fmt.Sprintf("not found: %s", e.targetName)
 }
+
+// DuplicateError
+type DuplicateError struct {
+	targetName string
+}
+
+func NewDuplicateError(targetName string) *DuplicateError {
+	return &DuplicateError{
+		targetName: targetName,
+	}
+}
+
+func (e *DuplicateError) Error() string {
+	return fmt.Sprintf("already exists: %s", e.targetName)
+}
